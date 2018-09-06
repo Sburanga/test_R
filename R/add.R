7cd41d26665911831510acb419fe7e41a780ef59
Stefano Toffol <stefanotoffol@gmail.com>
2018-09-06 17:16:30 +0200

    Deletion of a f***ing file

--- R/add.R
+++ R/add.R
@@ -1,4 +1,4 @@
-#' Title
+#' Add function
 #'
 #' @param x   First numeric value
 #' @param y   Second numeric value
@@ -7,6 +7,7 @@
 #' @export
 #'
 #' @examples add(3,4)
+
 add <- function(x,y) {
 
   if(!is.numeric(x) | !is.numeric(y)) stop("non-numeric argument to binary operator")
