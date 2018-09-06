d45b7ab7b2b413f4ec4f7c5e94b86c5afb5a84ca
Stefano Toffol <stefanotoffol@gmail.com>
2018-09-06 15:06:00 +0200

    First upload

---
+++ R/add.R
@@ -0,0 +1,18 @@
+# 7 + 8 = 15
+#
+# This is an example function named 'add'
+# which returns the sum of two numbers.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+add <- function(x,y) {
+  return(x+y)
+}
---
+++ tests/testthat/test_add.R
@@ -0,0 +1,17 @@
+context("add")
+
+test_that("Check types", {
+  expect_error(add("7a",5), "non-numeric argument to binary operator")
+  expect_equal(add(7,5), 12)
+})
+
+test_that("Check operations", {
+  expect_equal(add(2,9), 11)
+  expect_false(isTRUE(all.equal(add(2,9), 12)))
+})
+
+test_that("Check signs", {
+  expect_equal(add(-2,9), 7)
+  expect_false(isTRUE(all.equal(add(3,-7), 4)))
+  expect_equal(add(3,-3), 0 )
+})
